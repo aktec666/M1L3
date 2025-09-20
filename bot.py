24d02f458642a9b35590f877d832aa0240e4591a
Aleksey Kotets <aktec@mail.ru>
2025-09-20 15:22:42 +0300

    only admin ban

--- bot.py
+++ bot.py
@@ -16,9 +16,13 @@ def ban_user(message):
         user_id = message.reply_to_message.from_user.id
         user_status = bot.get_chat_member(chat_id, user_id).status 
          # проверка пользователя
+
+        check_id = message.from_user.id
+        status_id = bot.get_chat_member(chat_id, check_id).status 
+        
         if user_status == 'administrator' or user_status == 'creator':
             bot.reply_to(message, "Невозможно забанить администратора.")
-        else:
+        elif status_id == 'administrator' or status_id == 'creator':
             bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
